59141bb04a7d4f893ed81ad44b89c29ed9bb08b6
Gulnaz <madam.day@mail.ru>
2022-07-20 18:15:20 +0300

    added new branch

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+@Test
+public class CashbackHackServiceTest {
+    public void remain (){
+        CashbackHackService service =  new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void calcRemainIfAmount0(){
+        CashbackHackService service =  new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual,expected);
+
+    }
+
+
+
+}
